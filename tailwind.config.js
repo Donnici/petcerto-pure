26ee387017c75921361ce724e75180796045593a
Bruno Dias Donnici <bruno.dias.donnici@gmail.com>
2025-01-30 02:21:42 -0300

    feat: update home page

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+  content: [
+    './**/*.{js,jsx,ts,tsx,html}',
+  ],
+  presets: [],
+  theme: {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#027373',
+          'primary-dark': '#237b72',
+          secondary: '#E69D04',
+          accent: '#E69D04',
+          petcerto: '#40E0D0',
+          'green-dark': '#014141'
+        }
+      }
+    }
+  },
+  plugins: []
+}
